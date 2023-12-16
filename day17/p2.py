240bf024c8b8967d298db5c4f9d4c6a737f8f1ec
HuxleyBerry <huxley.berry@gmail.com>
2023-12-17 06:25:19 +0800

    day17

---
+++ day17/p2.py
@@ -0,0 +1,52 @@
+from queue import PriorityQueue
+
+def get_neighbours(x,y,direction, multiplicity, width, height):
+    if direction == None:
+        out_directions = [(1,0),(0,1)]
+    else:
+        out_directions = []
+        if multiplicity >= 4:
+            out_directions += [(direction[1], direction[0]), (-direction[1], -direction[0])]
+        if multiplicity < 10:
+            out_directions.append(direction)
+    return [(x+dx,y+dy) for (dx, dy) in out_directions if x+dx >= 0 and x+dx < width and y+dy >= 0 and y+dy < height]
+
+def print_path(predecessors, width, height):
+    passed_through = [[False for i in range(width)] for j in range(height)]
+    passed_through[height-1][width-1] = True
+    current = (width-1, height-1, final_dir, final_mult)
+    while (current[0], current[1]) != (0,0):
+        current = predecessors[current]
+        passed_through[current[1]][current[0]] = True
+    for line in passed_through:
+        s = ""
+        for b in line:
+            if b:
+                s += "#"
+            else:
+                s += "."
+        print(s)
+
+with open("input.txt") as file:
+    grid = [l.strip() for l in file]
+    width = len(grid[0])
+    height = len(grid)
+    visited = [[set() for char in line] for line in grid]
+    predecessors = {}
+    queue = PriorityQueue()
+    queue.put((0,(0,0),None,0))
+    while not queue.empty():
+        distance, (x,y), direction, multiplicity = queue.get()
+        if x == width-1 and y == height-1 and multiplicity >= 4:
+            final_dir, final_mult = direction, multiplicity
+            print(distance)
+            break
+        for x2, y2 in get_neighbours(x,y,direction, multiplicity, width, height):
+            new_direction = (x2-x,y2-y)
+            new_distance = distance + int(grid[y2][x2])
+            new_multiplicity = 1 if new_direction != direction else multiplicity + 1
+            if (new_direction, new_multiplicity) not in visited[y2][x2]:
+                visited[y2][x2].add((new_direction, new_multiplicity))
+                queue.put((new_distance, (x2,y2), new_direction, new_multiplicity))
+                predecessors[(x2,y2, new_direction, new_multiplicity)] = (x,y,direction, multiplicity)
+    #print_path(predecessors, width, height)        
